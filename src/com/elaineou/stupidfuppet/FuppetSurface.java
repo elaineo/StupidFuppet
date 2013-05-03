672c1662cd243f8514e7efed713b3e966adad176
elaineo <elaine.ou@gmail.com>
2013-05-03 16:55:00 -0700

    animated surfaceview

--- src/com/elaineou/stupidfuppet/FuppetSurface.java
+++ src/com/elaineou/stupidfuppet/FuppetSurface.java
@@ -6,6 +6,8 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Rect;
+import android.os.CountDownTimer;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -13,10 +15,39 @@ class FuppetSurface extends SurfaceView implements SurfaceHolder.Callback{
     private FuppetThread _thread;
     Bitmap _fface= BitmapFactory.decodeResource(getResources(),
             R.drawable.fuppet_head);
+    Bitmap _feye= BitmapFactory.decodeResource(getResources(),
+            R.drawable.fuppet_eye);
+    Bitmap _fmouth= BitmapFactory.decodeResource(getResources(),
+            R.drawable.fuppet_mouth);
+    Rect bgRect = new Rect(100,400,1500,1300);
+    Rect leRect = new Rect(500,650,600,750);
+    Rect reRect = new Rect(900,650,1000,750);
+    Rect moRect = new Rect(400,900,1200,1100);
+    
+    int i = 1;
     
     public FuppetSurface(Context context) {
         super(context);
         getHolder().addCallback(this);
+        
+        
+        final CountDownTimer t = new CountDownTimer(75000,100)
+        {
+            @Override
+            public void onFinish() {    
+            }
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                i++;
+                if ( i >= 4 ) { i = 1; }
+                if ( i == 1 ) { moRect.bottom = 1100; }
+                else if ( i == 2 ) { moRect.bottom = 1000; }
+                else if ( i == 3 ) { moRect.bottom = 930; }
+                else if ( i == 4 ) { moRect.bottom = 1000; }
+            }
+        }.start();
+        
         _thread = new FuppetThread(getHolder(), this);
     }
 
@@ -24,8 +55,10 @@ class FuppetSurface extends SurfaceView implements SurfaceHolder.Callback{
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawColor(Color.BLACK);
-        canvas.drawBitmap(_fface, 10, 10, null);
-
+        canvas.drawBitmap(_fface, null, bgRect, null);
+        canvas.drawBitmap(_feye, null, leRect, null);
+        canvas.drawBitmap(_feye, null, reRect, null);
+        canvas.drawBitmap(_fmouth, null, moRect, null);
     }
 
     public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
