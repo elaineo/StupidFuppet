d90558da11415ad1ac5f91d8aa1eb3520b87f251
elaineo <elaine.ou@gmail.com>
2013-05-08 14:04:19 -0700

    Switch to multimaps

---
+++ src/com/elaineou/stupidfuppet/FeedArticle.java
@@ -0,0 +1,10 @@
+package com.elaineou.stupidfuppet;
+
+import java.util.ArrayList;
+
+public class FeedArticle {
+	private String title;
+	private String link;
+	private String descr;
+	private ArrayList<String> tags; /* includes categories and keys */
+}
